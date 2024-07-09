9e3fec7a3da4973818a8135942b41764731d34b8
Ken Hauter <kenneth.hauter@gmail.com>
2024-07-09 16:14:11 +0900

    Change html buttons to include insert function

--- script.js
+++ script.js
@@ -1,56 +1,25 @@
-/*const display = document.querySelector('.display');
-const  numBtn = document.querySelectorAll('.numBtn');
+/*let operator = document.querySelectorAll('.operateBtn');
 
-
-let result = '';
-
-numBtn.forEach(btn => {
+operator.forEach(btn => {
     btn.addEventListener('click', (e) =>{
-        getNumBtn = e.target.innerHTML;
-        console.log(getNumBtn);
+        getOperatorBtn = e.target.innerHTML;
+        console.log(getOperatorBtn);
         
-        result = result + getNumBtn;
-        display.textContent = result;
+        return getOperatorBtn;
     });
-    
-});
 
-function clearNum() {
-    result.remove()
-}*/
+});*/
 
-
-
-/*let addNum = function(a,b) {
-return a + b;
-}
-console.log(addNum(1,68));
-
-let minusNum = function(a, b) {
-    return a - b;
-};
-
-console.log(minusNum(70,1));
-
-let multiplyNum = function(a, b) {
-    return a * b;
-};
-console.log(multiplyNum(6, 11));
-
-let divideNum = function(a, b) {
-    return a / b;
-};
-
-console.log(divideNum(66, 3));*/
 
 let operate = function(a, b, operator) {
+    
     switch(operator) {
-        case 'add' : return a + b
+        case 'add' :  return a + b
         case 'minus' : return a - b
-        case 'multiply' : return a * b
-        case 'divide' : return a / b 
+        case 'multiply' :  return a * b
+        case 'divide' :  return a / b 
     }
-        
+    
 } 
 
-console.log(operate(10, 3, 'add'))
+console.log(operate(5,5, 'add'))
