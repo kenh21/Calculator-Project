934b19232f5f342ad6e8e2c847e6e74d3eef0bc8
Ken Hauter <kenneth.hauter@gmail.com>
2024-07-10 16:00:45 +0900

    Change button layout and divs for cleaner look

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-/*let operator = document.querySelectorAll('.operateBtn');
-
-operator.forEach(btn => {
-    btn.addEventListener('click', (e) =>{
-        getOperatorBtn = e.target.innerHTML;
-        console.log(getOperatorBtn);
-        
-        return getOperatorBtn;
-    });
-
-});*/
+const numBtns = document.querySelectorAll('num');
+const operBtns = document.querySelectorAll('operator');
+const equalBtn = document.querySelector('operator-equals');
+const delBtn = document.querySelector('operator-del');
+const acBtn = document.querySelector('operator-ac');
+const pastOperationBtn = document.querySelector('past-operation');
+const currentOperationBtn = document.querySelector('current-operation');
 
 
-let operate = function(a, b, operator) {
-    
-    switch(operator) {
-        case 'add' :  return a + b
-        case 'minus' : return a - b
-        case 'multiply' :  return a * b
-        case 'divide' :  return a / b 
-    }
-    
-} 
-
-console.log(operate(5,5, 'add'))
