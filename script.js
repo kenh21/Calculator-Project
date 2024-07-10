3e04a2f7af2d05f0aafaf3f560d2bfd5aa0e103c
Ken Hauter <kenneth.hauter@gmail.com>
2024-07-10 16:58:22 +0900

    Add in functions for operations

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
+class calculator {
+    constructor(pastOperation, currentOperation) {
+        this.pastOperation = pastOperation
+        this.currentOperation = currentOperation
+        this.clearAll()
+    }
+
+clearAll() {
+    this.currentOperation = ''
+    this.pastOperation = ''
+    this.operation = undefined
+}
+delete() {
+
+}
+insertNum(num) {
+
+}
+
+selectOperation(operation) {
+
+}
+
+calculate() {
+
+}
+
+updateDisplay() {
+
+}
+}
+
+
 const numBtns = document.querySelectorAll('num');
 const operBtns = document.querySelectorAll('operator');
 const equalBtn = document.querySelector('operator-equals');
 const delBtn = document.querySelector('operator-del');
 const acBtn = document.querySelector('operator-ac');
-const pastOperationBtn = document.querySelector('past-operation');
-const currentOperationBtn = document.querySelector('current-operation');
+const pastOperation = document.querySelector('past-operation');
+const currentOperation = document.querySelector('current-operation');
 
 
